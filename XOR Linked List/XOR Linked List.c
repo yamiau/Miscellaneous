ddf62a5612417272af84e5150d92dbc11481b558
Yami <mu.meiraaz@gmail.com>
2020-06-10 20:51:47 -0300

    Started attempting to code a XOR Linked List for practicing

---
+++ XOR Linked List/XOR Linked List.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+    char[10] label;
+    struct node = next;
+} node;
+
+node * newList(char[10] label);
+
+int main()
+{
+
+}
+
+node * newList(char[10] label)
+{
+    node * head = NULL;
+    node *tail = NULL;
+
+}
+
